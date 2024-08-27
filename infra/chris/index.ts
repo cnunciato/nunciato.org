1acecc9a38a8551dcc3a0d6be1df2dc991a1a85a
Christian Nunciato <chris@nunciato.org>
2024-08-27 00:48:45 -0700

    Still not done, but getting there

--- infra/chris/index.ts
+++ infra/chris/index.ts
@@ -24,14 +24,49 @@ const attachment = new aws.iam.RolePolicyAttachment(
     },
 );
 
+const code = new pulumi.asset.AssetArchive({
+    ".": new pulumi.asset.FileArchive("../../out/apps/chris/dist"),
+    node_modules: new pulumi.asset.FileArchive("../../out/node_modules"),
+    "package.json": new pulumi.asset.FileAsset("../../out/package.json"),
+});
+
+const codeBucket = new aws.s3.Bucket("code-bucket");
+
+const ownershipControls = new aws.s3.BucketOwnershipControls(
+    "ownership-controls",
+    {
+        bucket: codeBucket.id,
+        rule: {
+            objectOwnership: "ObjectWriter",
+        },
+    },
+);
+
+const publicAccessBlock = new aws.s3.BucketPublicAccessBlock(
+    "public-access-block",
+    {
+        bucket: codeBucket.id,
+        blockPublicAcls: false,
+    },
+);
+
+const codeBucketObject = new aws.s3.BucketObject(
+    "app.zip",
+    {
+        bucket: codeBucket.id,
+        source: code,
+        acl: "public-read",
+    },
+    { dependsOn: [publicAccessBlock, ownershipControls] },
+);
+
 const lambda = new aws.lambda.Function("lambda-function", {
-    code: new pulumi.asset.AssetArchive({
-        ".": new pulumi.asset.FileArchive("../../apps/chris/dist"),
-    }),
     runtime: "nodejs20.x",
     role: role.arn,
     handler: "server/entry.handler",
     memorySize: 2048,
+    s3Bucket: codeBucket.id,
+    s3Key: codeBucketObject.id,
 });
 
 const permission = new aws.lambda.Permission("lambda-permission", {
