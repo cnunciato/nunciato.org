953a21afca04e5a85dae251caf444b9b8c604d33
Christian Nunciato <chris@nunciato.org>
2024-08-27 07:40:20 -0700

    Try AppRunner

--- apps/chris/astro.config.mjs
+++ apps/chris/astro.config.mjs
@@ -3,12 +3,14 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
-import serverless from "@repo/astro-adapter-serverless";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
     site: "https://chris.nunciato.org",
     integrations: [mdx(), sitemap(), tailwind(), preact()],
     output: "server",
-    adapter: serverless(),
+    adapter: node({
+        mode: "standalone",
+    }),
 });
--- infra/chris/index.ts
+++ infra/chris/index.ts
@@ -1,83 +1,39 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 
-const role = new aws.iam.Role("role", {
-    assumeRolePolicy: {
-        Version: "2012-10-17",
-        Statement: [
-            {
-                Action: "sts:AssumeRole",
-                Principal: {
-                    Service: "lambda.amazonaws.com",
-                },
-                Effect: "Allow",
+const appRunnerService = new aws.apprunner.Service("app", {
+    serviceName: "app",
+    sourceConfiguration: {
+        codeRepository: {
+            repositoryUrl: "https://github.com/cnunciato/nunciato.org",
+            sourceCodeVersion: {
+                type: "BRANCH",
+                value: "main",
             },
-        ],
+            codeConfiguration: {
+                configurationSource: "API",
+                codeConfigurationValues: {
+                    runtime: "NODEJS_20",
+                    buildCommand:
+                        "npm install && npm install --workspaces && npm run build",
+                    startCommand: "node apps/chris/dist/server/entry.mjs",
+                },
+            },
+        },
+        autoDeploymentsEnabled: true,
     },
-});
-
-const attachment = new aws.iam.RolePolicyAttachment(
-    "lambda-role-policy-attachment",
-    {
-        role: role,
-        policyArn: aws.iam.ManagedPolicy.AWSLambdaBasicExecutionRole,
+    instanceConfiguration: {
+        cpu: "1024",
+        memory: "2048",
     },
-);
-
-const code = new pulumi.asset.AssetArchive({
-    ".": new pulumi.asset.FileArchive("../../out/apps/chris/dist"),
-    node_modules: new pulumi.asset.FileArchive("../../out/node_modules"),
-    "package.json": new pulumi.asset.FileAsset("../../out/package.json"),
-});
-
-const codeBucket = new aws.s3.Bucket("code-bucket");
-
-const ownershipControls = new aws.s3.BucketOwnershipControls(
-    "ownership-controls",
-    {
-        bucket: codeBucket.id,
-        rule: {
-            objectOwnership: "ObjectWriter",
+    networkConfiguration: {
+        egressConfiguration: {
+            egressType: "DEFAULT",
+        },
+        ingressConfiguration: {
+            isPubliclyAccessible: true,
         },
     },
-);
-
-const publicAccessBlock = new aws.s3.BucketPublicAccessBlock(
-    "public-access-block",
-    {
-        bucket: codeBucket.id,
-        blockPublicAcls: false,
-    },
-);
-
-const codeBucketObject = new aws.s3.BucketObject(
-    "app.zip",
-    {
-        bucket: codeBucket.id,
-        source: code,
-        acl: "public-read",
-    },
-    { dependsOn: [publicAccessBlock, ownershipControls] },
-);
-
-const lambda = new aws.lambda.Function("lambda-function", {
-    runtime: "nodejs20.x",
-    role: role.arn,
-    handler: "server/entry.handler",
-    memorySize: 2048,
-    s3Bucket: codeBucket.id,
-    s3Key: codeBucketObject.id,
 });
 
-const permission = new aws.lambda.Permission("lambda-permission", {
-    action: "lambda:InvokeFunction",
-    principal: "apigateway.amazonaws.com",
-    function: lambda,
-});
-
-const api = new aws.apigatewayv2.Api("api", {
-    protocolType: "HTTP",
-    target: lambda.arn,
-});
-
-export const endpoint = api.apiEndpoint;
+export const serviceUrl = appRunnerService.serviceUrl;
