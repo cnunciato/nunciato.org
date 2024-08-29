433926d15384ec276c7d745ab5c3f7a99c1ff29e
Christian Nunciato <chris@nunciato.org>
2024-08-29 10:43:27 -0700

    Try fixing the image thign

--- apps/chris/astro.config.mjs
+++ apps/chris/astro.config.mjs
@@ -1,6 +1,4 @@
 import { defineConfig } from "astro/config";
-import { readFileSync, existsSync } from "fs";
-import { join } from "path";
 
 import mdx from "@astrojs/mdx";
 import node from "@astrojs/node";
@@ -10,11 +8,6 @@ import tailwind from "@astrojs/tailwind";
 import satori from "satori-astro";
 
 const output = "hybrid";
-let modulePath = "../../node_modules";
-
-if (!existsSync(modulePath)) {
-    modulePath = "node_modules";
-}
 
 // https://astro.build/config
 export default defineConfig({
--- apps/chris/src/components/MessageOfTheDay.tsx
+++ apps/chris/src/components/MessageOfTheDay.tsx
@@ -5,9 +5,7 @@ const message = signal("⏳");
 try {
     const data = await fetch("/api/message.json").then(response => response.json());
     message.value = data.message;
-} catch (error) {
-    console.error(error);
-}
+} catch (error) {}
 
 export default function MessageOfTheDay() {
     return <span>Message of the day: {message.value}</span>;
