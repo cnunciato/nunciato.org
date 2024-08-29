8c74aca3f5180d0af720dfc02db1f2e2b3aea86a
Christian Nunciato <chris@nunciato.org>
2024-08-29 09:15:42 -0700

    Boom -- generated images

--- apps/chris/src/pages/api/og.png.ts
+++ apps/chris/src/pages/api/og.png.ts
@@ -1,8 +1,31 @@
-import type { APIRoute } from "astro";
+import type { APIRoute, APIContext } from "astro";
+import { getCollection } from "astro:content";
 import { satoriAstroOG } from "satori-astro";
 import { html } from "satori-html";
 
-export const GET: APIRoute = async () => {
+export const prerender = false;
+
+export const GET: APIRoute = async ({ url }: APIContext) => {
+    // Pull the content type and ID from the querystring.
+    const contentType = url.searchParams.get("type");
+    const contentId = url.searchParams.get("id");
+
+    let title = "This is a title.";
+    let description = "It's awesome. You should check it out";
+
+    // Look 'em up.
+    if (contentType === "words") {
+        const words = await getCollection(contentType);
+        const post = words.find(w => w.slug === contentId);
+
+        if (post) {
+            title = post.data.title;
+            description = post.data.summary || post.data.description || "";
+        }
+    }
+
+    // Use the metadata to render the image.
+
     const fontFile = await fetch(
         "https://og-playground.vercel.app/inter-latin-ext-700-normal.woff",
     );
@@ -10,8 +33,8 @@ export const GET: APIRoute = async () => {
 
     return await satoriAstroOG({
         template: html`<div class="flex flex-col bg-gray-900 text-white text-4xl h-full px-12 py-8">
-            <h1 class="my-4 py-0">This is a title.</h1>
-            <p class="my-0">It's awesome. You should check it out.</p>
+            <h1 class="my-4 py-0">${title}</h1>
+            <p class="my-0">${description}</p>
         </div>`,
         width: 1200,
         height: 628,
