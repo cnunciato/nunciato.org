f381e5f85c453b2080b78bc42cda5c3d10636359
Christian Nunciato <chris@nunciato.org>
2024-09-20 16:41:28 -0700

    Dynamic pipeline, take 2

--- .buildkite/pipeline.js
+++ .buildkite/pipeline.js
@@ -1,15 +1,17 @@
 const pipeline = {
-    steps: {
-        commands: [
-            `echo "Hi, world!"`,
-            `export PATH="/.pulumi/bin:\$PATH"`,
-            `export PULUMI_ACCESS_TOKEN="$(buildkite-agent secret get PULUMI_ACCESS_TOKEN)"`,
-            `npm install && npm install --workspaces`,
-            `npm run build`,
-            `npm run test -w chris`,
-            `npm run $([ "$BUILDKITE_BRANCH" == "main" ] && echo "deploy" || echo "preview"):production -w infra.chris`,
-        ],
-    },
+    steps: [
+        {
+            commands: [
+                `echo "Hi, world!"`,
+                `export PATH="/.pulumi/bin:\$PATH"`,
+                `export PULUMI_ACCESS_TOKEN="$(buildkite-agent secret get PULUMI_ACCESS_TOKEN)"`,
+                `npm install && npm install --workspaces`,
+                `npm run build`,
+                `npm run test -w chris`,
+                `npm run $([ "$BUILDKITE_BRANCH" == "main" ] && echo "deploy" || echo "preview"):production -w infra.chris`,
+            ],
+        },
+    ],
 };
 
 console.log(pipeline);
