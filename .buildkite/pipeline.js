96be1d43375a1b160851bf1274637c726be163b6
Christian Nunciato <chris@nunciato.org>
2024-10-26 16:34:12 -0700

    Comment the parallelism

--- .buildkite/pipeline.js
+++ .buildkite/pipeline.js
@@ -8,9 +8,9 @@ const pipeline = {
                 `npm install && npm install --workspaces`,
                 `npm run build`,
                 `npm run test -w chris`,
-                // `npm run $([ "$BUILDKITE_BRANCH" == "main" ] && echo "deploy" || echo "preview"):production -w infra.chris`,
+                `npm run $([ "$BUILDKITE_BRANCH" == "main" ] && echo "deploy" || echo "preview"):production -w infra.chris`,
             ],
-            parallelism: 5,
+            // parallelism: 5,
         },
     ],
 };
