4ef0c8858654f5c5e44469ca4b0ad8ff2864a39d
Christian Nunciato <chris@nunciato.org>
2025-07-18 13:22:54 -0700

    Release maybe?

--- .buildkite/pipeline.js
+++ .buildkite/pipeline.js
@@ -45,7 +45,7 @@ pipeline.steps.push(
     ...[
         {
             label: `:hiking_boot: ${buildLabel} Chris's website`,
-            plugins: ["equinixmetal-buildkite/trivy"],
+            plugins: ["cnunciato/setup-pulumi"],
             commands: [
                 ...installAndBuildCommands,
                 `npm run $([ "$BUILDKITE_BRANCH" == "main" ] && echo "deploy" || echo "preview"):production -w infra.chris`,
