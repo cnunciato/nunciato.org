21f0d603d89839638f301a01c0edaf47e87a5c23
Christian Nunciato <chris@nunciato.org>
2025-07-18 13:13:56 -0700

    Tilde?

--- .buildkite/pipeline.js
+++ .buildkite/pipeline.js
@@ -45,7 +45,7 @@ pipeline.steps.push(
     ...[
         {
             label: `:hiking_boot: ${buildLabel} Chris's website`,
-            plugins: ["equinixmetal-buildkite/trivy", "cnunciato/setup-pulumi"],
+            plugins: [{ "praneetloke/setup-pulumi": "~" }],
             commands: [
                 ...installAndBuildCommands,
                 `npm run $([ "$BUILDKITE_BRANCH" == "main" ] && echo "deploy" || echo "preview"):production -w infra.chris`,
