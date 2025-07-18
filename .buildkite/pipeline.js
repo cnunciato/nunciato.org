e8c8c6925db3f07241a6ff98bf3320c0c5930415
Christian Nunciato <chris@nunciato.org>
2025-07-18 12:57:44 -0700

    Try another public plugin

--- .buildkite/pipeline.js
+++ .buildkite/pipeline.js
@@ -45,7 +45,7 @@ pipeline.steps.push(
     ...[
         {
             label: `:hiking_boot: ${buildLabel} Chris's website`,
-            plugins: ["praneetloke/setup-pulumi"],
+            plugins: ["equinixmetal-buildkite/trivy", "praneetloke/setup-pulumi"],
             commands: [
                 ...installAndBuildCommands,
                 `npm run $([ "$BUILDKITE_BRANCH" == "main" ] && echo "deploy" || echo "preview"):production -w infra.chris`,
