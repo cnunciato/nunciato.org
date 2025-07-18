a1ab563d6558db20fdd43b7e88db75d5adc6149f
Christian Nunciato <chris@nunciato.org>
2025-07-18 15:29:52 -0700

    Mine?

--- .buildkite/pipeline.js
+++ .buildkite/pipeline.js
@@ -45,7 +45,7 @@ pipeline.steps.push(
     ...[
         {
             label: `:hiking_boot: ${buildLabel} Chris's website`,
-            plugins: ["praneetloke/setup-pulumi#2979bb7"],
+            plugins: ["cnunciato/setup-pulumi#53d31a0"],
             commands: [
                 ...installAndBuildCommands,
                 `npm run $([ "$BUILDKITE_BRANCH" == "main" ] && echo "deploy" || echo "preview"):production -w infra.chris`,
