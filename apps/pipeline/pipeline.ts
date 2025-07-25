f6dcb6c35f524901d2393df2d44429b4047dd417
Christian Nunciato <chris@nunciato.org>
2025-07-25 07:08:38 -0700

    Slight refactor

--- apps/pipeline/pipeline.ts
+++ apps/pipeline/pipeline.ts
@@ -2,18 +2,23 @@ const { execSync } = require("child_process");
 import { Pipeline } from "@buildkite/buildkite-sdk";
 
 const pipeline = new Pipeline();
-// pipeline.agents = {
-//     queue: "hosted-linux",
-// };
 
-const installAndBuildCommands = [
-    `pulumi whoami`,
-
-    `echo "Installing workspaces..."`,
+const installAndBuild = [
     `npm install`,
     `npm install --workspaces`,
     `npm run lint`,
     `npm run build`,
+];
+
+const plugins: { [key: string]: any }[] = [
+    {
+        "praneetloke/setup-pulumi": {
+            use_oidc: true,
+            audience: "urn:pulumi:org:cnunciato",
+            pulumi_token_type: "urn:pulumi:token-type:access_token:personal",
+            pulumi_token_scope: "user:cnunciato",
+        },
+    },
 ];
 
 function touched(filePath: string) {
@@ -23,7 +28,6 @@ function touched(filePath: string) {
         .some((file: string) => file.includes(filePath));
 }
 
-// Determine if this is a PR or a push to main
 const isMain = process.env.BUILDKITE_BRANCH === "main";
 const action = isMain ? "deploy" : "preview";
 const label = isMain ? "Build and deploy" : "Build and preview for";
@@ -31,24 +35,15 @@ const label = isMain ? "Build and deploy" : "Build and preview for";
 // Build my site on every push.
 pipeline.addStep({
     label: `:hiking_boot: ${label} Chris's website`,
-    plugins: [
-        {
-            "praneetloke/setup-pulumi": {
-                use_oidc: true,
-                audience: "urn:pulumi:org:cnunciato",
-                pulumi_token_type: "urn:pulumi:token-type:access_token:personal",
-                pulumi_token_scope: "user:cnunciato",
-            },
-        },
-    ],
-    commands: [...installAndBuildCommands, `npm run ${action}:production -w infra.chris`],
+    plugins,
+    commands: [...installAndBuild, `npm run ${action}:production -w infra.chris`],
 });
 
 if (touched("apps/oliver")) {
     pipeline.addStep({
         label: `:pig: ${label} Oliver's website`,
-        plugins: ["praneetloke/setup-pulumi"],
-        commands: [...installAndBuildCommands, `npm run ${action}:production -w infra.oliver`],
+        plugins,
+        commands: [...installAndBuild, `npm run ${action}:production -w infra.oliver`],
     });
 }
 
